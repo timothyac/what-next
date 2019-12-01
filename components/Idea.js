606b64b143bc181950a61abd7e2a62a3d7421fd6
Austin Caron <aus10caron@icloud.com>
2019-12-01 13:46:02 -0500

    removed logic from idea comp and moved it to index

--- components/Idea.js
+++ components/Idea.js
@@ -1,50 +1,27 @@
-const array = [
-  {
-    name: "project generator"
-  },
-  {
-    name: "short-cut generator"
-  },
-  {
-    name: "short generator"
-  },
-  {
-    name: "cut generator"
-  },
-  {
-    name: "generator generator"
-  },
-  {
-    name: "number generator"
-  }
-];
+const Idea = props => {
+  let { name } = props.idea;
 
-const generateNewIdea = () => {
-  let numberOfIdeas = array.length;
-  let randomIdea = Math.floor(Math.random() * numberOfIdeas);
-  return array[randomIdea];
-};
+  return (
+    <div>
+      <div className="card">
+        <p>{name}</p>
+      </div>
 
-const Idea = () => (
-  <div>
-    <div className="card">
-      <p>{generateNewIdea().name}</p>
+      <style jsx>{`
+        .card {
+          border: 2px solid rgb(51, 51, 51);
+          border-radius: 10px;
+          padding: 2rem;
+        }
+
+        .card:hover {
+          cursor: pointer;
+          transition: border 0.2s ease 0s;
+          border: 2px solid #fff;
+        }
+      `}</style>
     </div>
-
-    <style jsx>{`
-      .card {
-        border: 2px solid rgb(51, 51, 51);
-        border-radius: 10px;
-        padding: 2rem;
-      }
-
-      .card:hover {
-        cursor: pointer;
-        transition: border 0.2s ease 0s;
-        border: 2px solid #fff;
-      }
-    `}</style>
-  </div>
-);
+  );
+};
 
 export default Idea;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,69 @@
+import React from "react";
 import Layout from "../components/Layout";
 import Idea from "../components/Idea";
 
-const Index = () => (
-  <Layout>
-    <div>
-      <h1>What Next?</h1>
-      <p>Generate a new project idea</p>
-      <Idea />
-    </div>
-  </Layout>
-);
+const array = [
+  {
+    name: "project generator"
+  },
+  {
+    name: "short-cut generator"
+  },
+  {
+    name: "short generator"
+  },
+  {
+    name: "cut generator"
+  },
+  {
+    name: "generator generator"
+  },
+  {
+    name: "number generator"
+  }
+];
 
-export default Index;
+// Generates new ideas
+const generateNewIdea = () => {
+  let numberOfIdeas = array.length;
+  let randomIdea = Math.floor(Math.random() * numberOfIdeas);
+  return array[randomIdea];
+};
+
+export default class Index extends React.Component {
+  state = {
+    idea: {
+      name: "test"
+    }
+  };
+
+  // Catches use pressing space bar button
+  catchSpaceButton = event => {
+    if (event.keyCode === 32) this.setState({ idea: generateNewIdea() });
+  };
+
+  componentDidMount() {
+    // Generate new idea on mount
+    this.setState({ idea: generateNewIdea() });
+
+    // Listen for keydown events
+    document.addEventListener("keydown", this.catchSpaceButton, false);
+  }
+
+  componentWillUnmount() {
+    // Remove event listener
+    document.removeEventListener("keydown");
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1>What Next?</h1>
+          <p>Press space to generate a new project idea!</p>
+          <Idea idea={this.state.idea} />
+        </div>
+      </Layout>
+    );
+  }
+}
